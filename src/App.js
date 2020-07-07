dc6bd2cb845f5c048287886e31b3b2cdef708c34
Giesing <thomas.giesing@metronom.com>
2020-07-07 16:44:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import data from "./data.json";
+import Products from './components/Products';
+
+
+class App extends React.Component {
+ constructor(){
+   super();
+   this.state = {
+    products: data.products,
+    size:"",
+    sort:"",
+   };
+ }
+  
+  render() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        <div className="content">
+          <div className ="main">
+            <Products products={this.state.products}></Products>
+          <div className ="sidebar"> Cart Items</div>
+        </div>
+      </div>
+      </main>
+      <footer>  All right is reserved (TG)  </footer>
+    </div> 
+  );
+ }
+}
+
+export default App;
